717d3ae1caa98cab7da1940bdeca92292879b8a3
guilleballesteros <bugfiher@outlook.com>
2020-10-28 09:42:38 +0100

    init commit

---
+++ app/assistance.php
@@ -0,0 +1,10 @@
+<?php
+//Ejemplo
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;         
+
+class assitance extends Model
+{ 
+    //pablo
+}
---
+++ database/factories/belongFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\belong::class, function (Faker $faker) {
+    return [
+        'student_id'=> \app\student::all()->random()->id,
+        'enterprice_id' => \app\enterprice::all()->random()->id,
+        'email'=> $faker->email,
+        'deleted' => $faker->boolean
+
+    ];
+});
