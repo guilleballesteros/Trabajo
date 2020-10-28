cfc098533c0d6642c3f28c7d0b1c3c3ec5cddcc5
guilleballesteros <bugfiher@outlook.com>
2020-10-28 10:05:36 +0100

    sobran modelos

--- database/factories/belongFactory.php
+++ database/factories/belongFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\belong::class, function (Faker $faker) {
     return [
-        //
+        'student_id'=> \app\student::all()->random()->id,
+        'enterprice_id' => \app\enterprice::all()->random()->id,
+        'email'=> $faker->email,
+        'deleted' => $faker->boolean
+
     ];
 });
