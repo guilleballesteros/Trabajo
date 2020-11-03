cf442f83510d05e3fe6210dbdfb81b07d27768ac
pablodelgadom <pablo00dm00@gmail.com>
2020-11-03 12:39:57 +0100

    rty

--- app/enterprise.php
+++ app/enterprise.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class enterprise extends Model
 {
-    //
-    protected $fillable = ['id', 'name', 'email','deleted'];
+    protected $table='enterprises';
+    protected $fillable = ['name', 'email','deleted'];
+
+    public function enterprises(){
+        return $this->hasMany(belong::class);
+       }
+       
 }
--- app/worksheet.php
+++ app/worksheet.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class worksheet extends Model
 {
-    //
+    protected $table='worksheets';
+    protected $fillable = [
+        'date','description','student_id','accepted','deleted'
+    ];
 }
