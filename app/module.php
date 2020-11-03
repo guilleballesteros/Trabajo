3f1ffa40c8289c2db65ac89984160fb970dc84ec
guilleballesteros <bugfiher@outlook.com>
2020-11-03 13:05:57 +0100

    cambio

--- app/module.php
+++ app/module.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class module extends Model
 {
-    
+    protected $table='modules';
+    protected $fillable=[
+        'name','cycle_id','deleted'
+    ];
+
+    public function ras(){
+        return $this->hasMany(ra::class);
+    }
+    public function cycle(){
+        return $this->belongsTo(cycle::class,'cycle_id');
+    }
 }
 
