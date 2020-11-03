d0a0b1e289196e90def1983a51a745abea4d7b75
guilleballesteros <bugfiher@outlook.com>
2020-11-03 13:02:19 +0100

    cambio

--- app/ra.php
+++ app/ra.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class ra extends Model
 {
     protected $table='ras';
-    
+    protected $fillable=[
+        'number','description','module_id','deleted'
+    ];
+    public function ces(){
+        return $this->hasMany(ce::class);
+    }
+    public function module(){
+        return $this->belongsTo(module::class,'module_id');
+    }
 }
