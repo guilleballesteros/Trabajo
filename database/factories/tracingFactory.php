8b30ae432fc1d58124b02f3e4fde70556aa6dcd7
guilleballesteros <bugfiher@outlook.com>
2020-11-04 08:48:03 +0100

    cambios

--- database/factories/tracingFactory.php
+++ database/factories/tracingFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\tracing::class, function (Faker $faker) {
     return [
-        
+         'type'=>$faker->sentence,
+         'reason'=>$faker->sentence,
+         'observation'=>$faker->sentence,
+         'tutor_c_id'=>\app\Tutor_c::all()->random()->id,
+         'deleted'=>$faker->boolean
     ];
 });
