47191cffd61d852ad40d6cf40d588589a028fbdc
guilleballesteros <bugfiher@outlook.com>
2020-11-09 13:30:42 +0100

    cambios

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,13 @@
 
 Route::get('/', function () {
     // return view('welcome');
+    return view('auth.login');
+});
+Route::get('/admin', function (){
     return view('admin.dashboard');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('student', 'StudentController');
 
