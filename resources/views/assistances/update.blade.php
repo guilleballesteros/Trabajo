ddeb7ae0e618d386289bca49bc971393269232a7
YerayDP <yerayd99@gmail.com>
2020-11-22 19:56:39 +0100

    Yeray

--- resources/views/assistances/update.blade.php
+++ resources/views/assistances/update.blade.php
@@ -22,7 +22,7 @@
 
 			    <div class="panel panel-default">
     				<div class="panel-heading">
-	    				<h3 class="panel-title">Creating a new Assistance</h3>
+	    				<h3 class="panel-title">Updating a new Assistance</h3>
 		    		</div>
 			    	<div class="panel-body">					
     					<div class="table-container">
